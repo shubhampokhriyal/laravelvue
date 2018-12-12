9af54cc1a19d15a952796207f386f2fb6d9f53f1
shubham <shubhampokhriyal@hotmail.com>
2018-12-12 18:01:44 +0530

    updates

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,39 +6,23 @@
  */
 
 require('./bootstrap');
-
+// routers
 window.Vue = require('vue');
 
 import VueRouter from 'vue-router'
-Vue.use(VueRouter)
-
 import routes from './router';
+import { Form, HasError, AlertError } from 'vform'
 
+Vue.use(VueRouter)
 const router = new VueRouter({
     mode: 'history',
     routes,
 });
-
-// export default router;
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key)))
-
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+/***************************************************vform */
+window.Form= Form
+Vue.component(HasError.name, HasError)
+Vue.component(AlertError.name, AlertError)
+/***************************************************vform */
 
 
 const app = new Vue({
--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -8,7 +8,7 @@
                 <h3 class="card-title">Users Table</h3>
 
                 <div class="card-tools">
-                    <button class="btn btn-success">Add new <i class="fas fa-user-plus fa-fw"></i></button>
+                    <button class="btn btn-success" data-toggle="modal" data-target="#addNew">Add new <i class="fas fa-user-plus fa-fw"></i></button>
                 </div>
               </div>
               <!-- /.card-header -->
@@ -43,11 +43,80 @@
             <!-- /.card -->
           </div>
         </div>
+        <!-- Modal -->
+        <div class="modal fade" id="addNew" tabindex="-1" role="dialog" aria-labelledby="addNewLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="addNewLabel">Add new</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form @submit.prevent="createUser" @keydown="form.onKeydown($event)"  autocomplete="off">
+            <div class="modal-body">
+                <div class="form-group">
+                    <input autocomplete="off" v-model="form.name" type="text" name="name" class="form-control" :class="{ 'is-invalid': form.errors.has('name') }" placeholder="Name">
+                    <has-error :form="form" field="name"></has-error>
+                </div>
+
+                <div class="form-group">
+                    <input autocomplete="off" v-model="form.email" type="text" name="email" class="form-control" :class="{ 'is-invalid': form.errors.has('email') }" placeholder="Email">
+                    <has-error :form="form" field="email"></has-error>
+                </div>
+                <div class="form-group">
+                    <textarea autocomplete="off" v-model="form.bio" name="bio" class="form-control" :class="{ 'is-invalid': form.errors.has('bio') }" placeholder="Bio (Optional)"></textarea>
+                    <has-error :form="form" field="bio"></has-error>
+                </div>
+                <div class="form-group">
+
+                    <select autocomplete="off" v-model="form.type" name="type" class="form-control" :class="{ 'is-invalid': form.errors.has('type') }" >
+                        <option value="">Select User Type</option>
+                        <option value="admin">Admin</option>
+                        <option value="user">User</option>
+                        <option value="auther">Auther</option>
+                    </select>
+                    <has-error :form="form" field="email"></has-error>
+                </div>
+                <div class="form-group">
+                    <input autocomplete="off" v-model="form.password" type="password" name="password" class="form-control" :class="{ 'is-invalid': form.errors.has('password') }" placeholder="password">
+                    <has-error :form="form" field="password"></has-error>
+                </div>
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Create</button>
+            </div>
+            </form>
+            </div>
+        </div>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                    form: new Form({
+                        name: '',
+                        email: '',
+                        type: '',
+                        bio: '',
+                        photo: '',
+                    })
+                    }
+        },
+        methods:{
+            createUser () {
+                // Submit the form via a POST request
+                this.form.post('api/user')
+                    .then(
+                        ({ data }) => { console.log(data) }
+                        )
+                }
+        },
         mounted() {
             console.log('Component mounted.')
         }
