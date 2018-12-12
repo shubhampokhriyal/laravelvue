51e21b06ed5a3ffeb3301f9841a59f3ee2684700
shubham <shubhampokhriyal@hotmail.com>
2018-12-12 16:08:10 +0530

    edits

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -188,6 +188,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
               </li>
             </ul>
           </li>
+
         <li class="nav-item">
             <router-link to="/profile" class="nav-link">
               <i class="nav-icon fas fa-user orange"></i>
